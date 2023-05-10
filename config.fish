60db8923f6ade0e3dbc7526462523438191f76b4
Chengke Wang <chengke@pku.edu.cn>
2023-05-10 17:11:08 +0800

    add fish shell

---
+++ config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -U fish_user_paths ~/.local/bin/ $fish_user_paths
+    alias proxyon 'export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890'
+end
